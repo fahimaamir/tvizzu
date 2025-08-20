fa6e2a4a4a743072656a97b00beeba77d483c53c
fahimaamir <fahimaamir72@gmail.com>
2025-08-20 13:22:40 +0500

    Add files via upload

---
+++ tvizzu.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+from typing import List
+import numpy as np
+import pandas as pd
+import streamlit as st
+from streamlit_vizzu import Config, Data, Style, VizzuChart
+from ipyvizzustory import Slide, Step
+from ipyvizzustory.env.py.story import Story
+
+
+st.header('Muhammad is the Best')
